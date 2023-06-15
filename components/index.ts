a8387c890cf50116251359d180fa6660d2b0d9e7
phoenixjw <phoenixjw9320@gmail.com>
2023-06-15 11:45:23 +0100

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export {
+    Hero, CustomButton, Navbar, Footer
+}
+
+
+// index file allows for cleaner component imports throughout our code
