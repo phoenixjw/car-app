9150bc3030aab12a3ef18b9f622a336c6d7a7e37
phoenixjw <phoenixjw9320@gmail.com>
2023-06-15 11:55:37 +0100

    Created initial searchbar and custom filter

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,11 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
 export {
-    Hero, CustomButton, Navbar, Footer
+    Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter
 }
 
 
